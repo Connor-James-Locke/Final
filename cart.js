5da31f0c51a916d2bb9f3e00ddbf0b241bec2ac6
Lockeness1 <connoslock@gmail.com>
2023-05-09 09:00:20 -0500

    A lot of things honestly, haven't commited in a while

--- cart.js
+++ cart.js
@@ -1,1 +1,31 @@
 "use strict"
+
+window.addEventListener("load", function() {
+var checkout = document.forms.checkout;
+
+calculate();
+
+checkout.elements.shoe.onchange = calculate;
+checkout.elements.insurance.onchange = calculate;
+checkout.elements.shipping.onchange = calculate;
+
+var shippingCalc = document.querySelectorAll('input[name="shipping"]');
+for (var i = 0; i < shippingCalc.length; i++) {
+    shippingCalc[i].onclick = calculate;
+}
+});
+
+function calculate() {
+    var checkout = document.forms.checkout;
+
+    var shoeIndex = checkout.elements.shoe.selectedIndex;
+    var shoeCost = checkout.elements.shoe.options[shoeIndex].value;
+    var insuranceIndex = checkout.elements.insurance.selectedIndex;
+    var insuranceCost = checkout.elements.insurance.options[insuranceIndex].value;
+
+    var shippingCost = document.querySelector('input[name="shipping"]:checked').value
+
+    var total = parseFloat(shoeCost) + parseFloat(insuranceCost) + parseFloat(shippingCost)
+    checkout.elements.total.value = total
+    console.log(shoeCost, insuranceCost, shippingCost, total)
+}
